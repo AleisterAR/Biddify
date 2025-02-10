8c454f5681ae505f1bae9324e83f1ae9f4d5df44
Thu Ta <thuta.ai3817@gmail.com>
2025-02-10 23:48:57 +0900

    bug fix

--- bid/context_processors.py
+++ bid/context_processors.py
@@ -1,6 +1,7 @@
 from bid.models import Notification
 
 def notification_processor(request):
+    notifications, has_unread = [], False
     if request.user.is_authenticated:
         notifications = Notification.objects.filter(user=request.user).order_by("-created_at")
         has_unread = notifications.filter(is_read=False).count() > 0
