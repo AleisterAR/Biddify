46dcca5e3db1f64f18c29ec9dd33f8dd72b8d0c7
Thu Ta <thuta.ai3817@gmail.com>
2025-02-15 23:40:39 +0900

    ui changes and update

--- bid/urls.py
+++ bid/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from bid.views import create_auction, delete_notification, delete_all_notifications, mark_as_read
+from bid.views import create_auction, delete_notification, delete_all_notifications, mark_as_read, feature_auctions
 
 urlpatterns = [
     path("create_auction/<int:item_id>", create_auction, name="create_auction"),
     path("delete_notification/<str:notification_message>", delete_notification, name="delete_notification"),
     path("delete_all_notifications", delete_all_notifications, name="delete_all_notifications"),
-    path("mark_as_read/<str:notification_message>", mark_as_read, name="mark_as_read")
+    path("mark_as_read/<str:notification_message>", mark_as_read, name="mark_as_read"),
+    path("featured_auctions", feature_auctions, name="featured_auctions")
 ]
--- bid/views.py
+++ bid/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from bid.forms import AuctionForm
-from bid.models import Auction, Notification
+from bid.models import Auction, Notification, Bid
 from item.models import Item
 from django.contrib import messages
 import json
 from django_htmx.http import trigger_client_event
 from django.http import HttpResponse
+from django.db.models import Q, Max, F
 
 # Create your views here.
 def create_auction(request, item_id):
@@ -41,4 +42,7 @@ def mark_as_read(request, notification_message):
     new_noti.save()
     has_unread = Notification.objects.filter(user=request.user, is_read=False).count() > 0
     return render(request, 'utilities/partials/read_notification.html', context={"notification": new_noti, "has_unread": has_unread})
-    
+
+def feature_auctions(request):
+    auctions = Auction.objects.prefetch_related('bids').annotate(max_bid = Max('bids__bid_amount')).exclude(max_bid=None).order_by('-max_bid')
+    return render(request, 'utilities/partials/featured_auctions.html', context={'featured_auctions':auctions})
